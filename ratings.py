9e082c5020e9653adfa06522a5d6031ecaf468dd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 17:51:04 -0700

    Solved!

--- ratings.py
+++ ratings.py
@@ -4,24 +4,23 @@
 # put your code here
 
 def get_rating(filename):
+    """Gets restaurant ratings"""
 
     ratings_dict = {}
 
     for line in open(filename):
         list_of_rests = line.rstrip().split(":")
 
-        # creating a dictionary with restaurnt [0], and rating [1]
+        # creates a dictionary with restaurant [0], and rating [1]
         ratings_dict[list_of_rests[0]] = list_of_rests[1]
 
-        #print "{} is rated at {}".format(list_of_rests[0], list_of_rests[1])
+    # creates a sorted list of tuples
+    new_tuple_list = ratings_dict.items()
+    new_sorted_list = sorted(new_tuple_list)
 
-        new_tuple_list = ratings_dict.items()
-        new_sorted_list = sorted(new_tuple_list)
-
+    #prints restaurant, rating in alphabetical order
     for pair in new_sorted_list:
         print "{} is rated at {}".format(pair[0], pair[1])
 
-    return ratings_dict
 
-
-ratings = get_rating("scores.txt")
+get_rating("scores.txt")
